6710899368f13a9c7c13bbb62a3f7a26fde21ca8
opmiss <opmiss@gmail.com>
2015-04-08 17:25:47 -0700

    number solitaire

--- src/com/codility/lessons/dp/NumberSolitaire.java
+++ src/com/codility/lessons/dp/NumberSolitaire.java
@@ -1,6 +1,10 @@
 package com.codility.lessons.dp;
+
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+/* In a given array, find the subset of maximal sum in which the distance between consecutive elements is at most 6. */
 
 public class NumberSolitaire {
 	public int solution(int[] A){
@@ -8,6 +12,7 @@ public class NumberSolitaire {
 		dp[0] = A[0]; 
 		int prev = dp[0]; 
 		for (int i=1; i<A.length; i++){
+			prev = dp[i-1]; 
 			for (int k=1; k<=6; k++){ 
 				int id = i-k;
 				if (id>=0) prev = Math.max(prev, dp[id]); 
@@ -21,6 +26,14 @@ public class NumberSolitaire {
 	public void test(){
 		int[] A = new int[]{1, -2, 0, 9, -1, -2}; 
 		assertEquals(8, solution(A)); 
+		A = new int[]{1, 2, 3, 4, 1, 2}; 
+		assertEquals(13, solution(A));
+		A = new int[]{-1, -2, -3, -4, -1, -2}; 
+		assertEquals(-3, solution(A));
+		A = new int[]{1, -2, 3, -4, 1, 2}; 
+		assertEquals(7, solution(A));
+		A = new int[]{-1, -1, -1, -1, -1, -1, -1, 1, 1, 1}; 
+		assertEquals(1, solution(A)); 
 	}
 	
 }
