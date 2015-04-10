d0098cdb2d832d2e9f6ce35f29ea3922606b00bc
opmiss <opmiss@gmail.com>
2015-04-09 20:27:59 -0700

    min absolute sum

--- src/com/codility/lessons/dp/MinAbsSum.java
+++ src/com/codility/lessons/dp/MinAbsSum.java
@@ -1,28 +1,49 @@
 package com.codility.lessons.dp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /*
- For a given array A of N integers and a sequence S of N integers from the set {−1, 1}, we define val(A, S) as follows:
- 	val(A, S) = |sum{ A[i]*S[i] for i = 0..N−1 }|
- For a given array A, we are looking for such a sequence S that minimizes val(A,S).
- Write a function:
- 	int solution(int A[], int N);
- that, given an array A of N integers, 
- computes the minimum value of val(A,S) from all possible values of val(A,S) for all possible sequences S of N integers from the set {−1, 1}.
- For example, given array:
-  A[0] =  1
-  A[1] =  5
-  A[2] =  2
-  A[3] = -2
- your function should return 0, since for S = [−1, 1, −1, 1], val(A, S) = 0, which is the minimum possible value.
-Assume that:
-N is an integer within the range [0..20,000];
-each element of array A is an integer within the range [−100..100].
-Complexity:
-	expected worst-case time complexity is O(N*max(abs(A))2);
-	expected worst-case space complexity is O(N+sum(abs(A))), beyond input storage (not counting the storage required for input arguments).
-	Elements of input arrays can be modified.
+Given array of integers, find the lowest absolute sum of elements.
+test result
+https://codility.com/demo/results/demoGE3VDQ-SC8/
 */
 public class MinAbsSum {
 	public int solution(int[] A){
-		return 0; 
+		int S = 0; 
+		int[] C = new int[101]; 
+		int M = 0; 
+		for (int i=0; i<A.length; i++){
+			if (A[i]<0) A[i] = -A[i];
+			S+=A[i]; 
+			C[A[i]]++;
+			M = (M<A[i])?A[i]:M; 
+		}
+		int[] R = new int[S+1]; 
+		R[0] = 1; 
+		for (int i=1; i<=M; i++){
+			if (C[i]==0) continue; 
+			for (int j=0; j<S+1; j++){
+				if (R[j]>0) R[j]=C[i]+1; 
+				else if (j-i>=0 && R[j-i]>0) R[j] = R[j-i]-1; 
+			}
+		}
+		int ret = S; 
+		for (int i=0; i<S/2+1; i++){
+			if (R[i]>0) ret = Math.min(ret, S-i*2); 
+		}
+		return ret; 
+	}
+	@Test
+	public void test(){
+		int[] A = new int[]{1, 5, 2, -2}; 
+		assertEquals(0, solution(A)); 
+		A = new int[]{3, 3, 5, 6}; 
+		assertEquals(1, solution(A)); 
+		A = new int[]{1, 1};
+		assertEquals(0, solution(A)); 
+		A = new int[]{0, 0, 0, 0}; 
+		assertEquals(0, solution(A));  
 	}
 }
