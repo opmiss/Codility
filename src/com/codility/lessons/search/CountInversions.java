2d3dcc58fba065aef416067c7eb5f9c26d2ba551
opmiss <opmiss@gmail.com>
2015-05-05 09:57:07 -0700

    Count Inversions

--- src/com/codility/lessons/search/CountInversions.java
+++ src/com/codility/lessons/search/CountInversions.java
@@ -2,28 +2,14 @@ package com.codility.lessons.search;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import org.junit.*;
 
 /*  
-    A zero-indexed array A consisting of N integers is given. 
-    An inversion is a pair of indexes (P, Q) such that P < Q and A[Q] < A[P].
-Write a function: 
-	int solution(int A[], int N);
-	that computes the number of inversions in A, or returns −1 if it exceeds 1,000,000,000.
-Assume that:
-	N is an integer within the range [0..100,000];
-	each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
-	For example, in the following array:
-	A[0] = -1 A[1] = 6 A[2] = 3 A[3] =  4 A[4] = 7 A[5] = 4
-	there are four inversions:
-  	(1,2)  (1,3)  (1,5)  (4,5)
-  	so the function should return 4.
-Complexity:
-	expected worst-case time complexity is O(N*log(N)); *! This is wrong, the worst case can output O(N*N) pairs*
-	expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
-	Elements of input arrays can be modified.
+ * Compute number of inversion in an array
+ * https://codility.com/demo/results/demoC9ZEP6-76H/
 */
 
 public class CountInversions {
@@ -32,7 +18,7 @@ public class CountInversions {
 	
 	public int solution(int[] A){
 		count=0; 
-		merge(A, 0, A.length-1); 
+		mergeSort(A, 0, A.length-1); 
 		return count; 
 	}
 	
@@ -43,6 +29,7 @@ public class CountInversions {
 				int t = A[start]; 
 				A[start] = A[end]; 
 				A[end] = t; 
+				count++; 
 			}
 			return; 
 		}
@@ -53,7 +40,6 @@ public class CountInversions {
 	}
 	
 	public void merge(int[] A, int start, int end){
-		System.out.println(A.toString()); 
 		int mid = (start+end)/2; 
 		int n1 = mid-start+1; 
 		int n2 = end-mid; 
@@ -66,7 +52,6 @@ public class CountInversions {
 			if (A2[i2]<A1[i1]) {
 				A[i++]=A2[i2++]; 
 				count+=n1-i1; 
-				System.out.println(count); 
 				if (count>1000000000) count=-1; 
 			}
 			else {
@@ -90,6 +75,6 @@ public class CountInversions {
 		A = new int[]{5, 4, 3, 2, 1}; 
 		assertEquals(10, solution(A)); 
 		A = new int[]{1, 2, 3, 4, 5}; 
-		assertEquals(0, solution(A)); 
+		assertEquals(0, solution(A));
 	}
 }
